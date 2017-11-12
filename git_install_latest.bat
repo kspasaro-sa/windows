4af71fa09a3e8484f7f8a822945dae5494a0aeea
Kim Spasaro <32518752+kspasaro-sa@users.noreply.github.com>
2017-11-12 10:05:26 -0600

    Adding local vs buildtool intaller

--- git_install_latest.bat
+++ git_install_latest.bat
@@ -1,7 +1,7 @@
 REM Install git for command prompt
 set "git_src=https://github.com/git-for-windows/git/releases/download/v2.15.0.windows.1/Git-2.15.0-64-bit.exe"
 set "git_path=%PROGRAMFILES%\Git"
-set "git_installer=git-install.exe"
+set "git_installer=git-installer.exe"
 if not exist "%git_path%" mkdir "%git_path%"
 powershell -Command "(New-Object Net.WebClient).DownloadFile('%git_src%', '%git_path%\%git_installer%')"
 set "PATH=%PATH%;%git_path%"
---
+++ vs_buildtools_install_2015.bat
@@ -0,0 +1,8 @@
+REM Install vs_buildtools for command prompt
+set "vs_buildtools_src=https://download.microsoft.com/download/E/E/D/EEDF18A8-4AED-4CE0-BEBE-70A83094FC5A/BuildTools_Full.exe"
+set "vs_buildtools_path=%PROGRAMFILES%\vs_buildtools"
+set "vs_buildtools_installer=vs-buildtools-installer.exe"
+if not exist "%vs_buildtools_path%" mkdir "%vs_buildtools_path%"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('%vs_buildtools_src%', '%vs_buildtools_path%\%vs_buildtools_installer%')"
+set "PATH=%PATH%;%vs_buildtools_path%"
+%vs_buildtools_installer%
