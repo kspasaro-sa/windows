0ee8ea6e887357ee05e22e2447309849f71bbc58
Kim Spasaro <32518752+kspasaro-sa@users.noreply.github.com>
2017-11-12 16:59:05 -0600

    new file

---
+++ msbuild_add_path.bat
@@ -0,0 +1,8 @@
+set query=reg query "HKLM\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full" /v "InstallPath"
+for /f "tokens=*" %%i IN (' %query% ') DO SET result=%%i
+set result=( %result% )
+setlocal enabledelayedexpansion
+for %%i in %result% do (
+  set msbuild_dir=%%i
+)
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /f /v Path /t REG_SZ /d "%PATH%;%msbuild_dir%"
